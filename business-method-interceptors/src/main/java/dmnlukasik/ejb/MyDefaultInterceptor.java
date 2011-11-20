b241d89fc13adc39720acec8142fc7f284c093ca
Damian Lukasik <copernic777@gmail.com>
2011-11-20 13:59:32 +0100

    interceptor lifecycle callback

--- business-method-interceptors/src/main/java/dmnlukasik/ejb/MyDefaultInterceptor.java
+++ business-method-interceptors/src/main/java/dmnlukasik/ejb/MyDefaultInterceptor.java
@@ -1,5 +1,6 @@
 package dmnlukasik.ejb;
 
+import javax.annotation.PostConstruct;
 import javax.interceptor.InvocationContext;
 
 public class MyDefaultInterceptor {
@@ -8,5 +9,17 @@ public class MyDefaultInterceptor {
         System.out.println("MyDefaultInterceptor intercepting: " +
                 ic.getTarget().getClass().getSimpleName() + "." + ic.getMethod().getName());
         return ic.proceed();
+    }
+
+    @PostConstruct
+    public void postConstruct(InvocationContext ic)
+            throws Exception {
+        System.out.println(" MyDefaultInterceptor.postConstruct");
+        /**
+         * Important!
+         * Must call proceed, in order for the other interceptor methods
+         * in the interceptor chain to become invoked.
+         */
+        ic.proceed();
     }
 }
