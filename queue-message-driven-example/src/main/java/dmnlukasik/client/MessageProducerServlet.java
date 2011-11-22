37fcbc352bee47ac9f96e1ef1c69724033d4104f
Damian Lukasik <copernic777@gmail.com>
2011-11-22 19:09:38 +0100

    jms queue

---
+++ queue-message-driven-example/src/main/java/dmnlukasik/client/MessageProducerServlet.java
@@ -0,0 +1,89 @@
+package dmnlukasik.client;
+
+import dmnlukasik.ejb.MyMessage;
+
+import javax.annotation.Resource;
+import javax.jms.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
+
+@WebServlet(name = "MessageProducerServlet", urlPatterns = "/test.do")
+public class MessageProducerServlet extends HttpServlet {
+
+    private static final long serialVersionUID = -4364474814559146703L;
+
+    @Resource(mappedName = "jms/QueueConnectionFactory")
+    private ConnectionFactory connectionFactory;
+
+    @Resource(mappedName = "jms/QueueDestination")
+    private Queue queue;
+
+    private AtomicLong messageNumber = new AtomicLong(0);
+
+    public MessageProducerServlet() {
+        super();
+        System.out.println("*** MessageProducerServlet created");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter responseWriter = response.getWriter();
+
+        try {
+            sendJmsMessage();
+            responseWriter.println("A message was sent at " + new Date());
+        } catch (JMSException theException) {
+            responseWriter.println("An error occurred sending message: " + theException);
+        }
+    }
+
+    private void sendJmsMessage() throws JMSException {
+        MessageProducer messageProducer;
+        Connection connection = null;
+        try {
+            /* Retrieve a JMS connection from the queue connection factory. */
+            connection = connectionFactory.createConnection();
+            /*
+             * Create the JMS session; not transacted and with auto-acknowledge.
+             */
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            /* Create a JMS message producer for the queue destination. */
+            messageProducer = session.createProducer(queue);
+            /* Create the object to be sent in the message created above. */
+            MyMessage myMessage = new MyMessage();
+            myMessage.setMessageNumber(messageNumber.incrementAndGet());
+            myMessage.setMessageString("Hello Message Driven Beans");
+            myMessage.setMessageTime(new Date());
+            /* Create message used to send a Java object. */
+            ObjectMessage objectMessage = session.createObjectMessage();
+            objectMessage.setObject(myMessage);
+            /* Send the message. */
+            messageProducer.send(objectMessage);
+        } finally {
+            closeJmsResources(connection);
+        }
+    }
+
+    /**
+     * Closes the supplied JMS connection if it is not null.
+     * If supplied connection is null, then do nothing.
+     *
+     * @param connection JMS connection to close.
+     */
+    private void closeJmsResources(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (JMSException ignored) {
+            }
+        }
+    }
+}
---
+++ queue-message-driven-example/src/main/java/dmnlukasik/ejb/QueueListenerEJB.java
@@ -0,0 +1,44 @@
+package dmnlukasik.ejb;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+}, mappedName = "jms/QueueDestination", name = "QueueListener1")
+public class QueueListenerEJB implements MessageListener {
+
+    private static AtomicInteger mCurrentBeanNumber = new AtomicInteger(0);
+
+    private int mBeanNumber = mCurrentBeanNumber.incrementAndGet();
+
+
+    public QueueListenerEJB() {
+        System.out.println("*** QueueListenerEJB created: " + mBeanNumber);
+    }
+
+    @Override
+    public void onMessage(Message inMessage) {
+        System.out.println("*** Bean " + mBeanNumber + " received message: " + inMessage);
+        extractMessagePayload(inMessage);
+    }
+
+    private void extractMessagePayload(Message inMessage) {
+        if (inMessage instanceof ObjectMessage) {
+            try {
+                ObjectMessage theObjMsg = (ObjectMessage) inMessage;
+                MyMessage theMsgPayload = (MyMessage) theObjMsg.getObject();
+                System.out.println("Received message with number: " + theMsgPayload.getMessageNumber());
+                System.out.println("   Message string: " + theMsgPayload.getMessageString());
+                System.out.println("   Message time: " + theMsgPayload.getMessageTime());
+            } catch (JMSException theException) {
+                System.out.println("An error occurred retrieving message payload: " + theException);
+            }
+        }
+    }
+}
