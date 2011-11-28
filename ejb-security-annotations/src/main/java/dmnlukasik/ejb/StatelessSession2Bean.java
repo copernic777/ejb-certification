5982fe1f9717e83908c6e58a8a4a412d054120b9
Damian Lukasik <copernic777@gmail.com>
2011-11-28 20:06:01 +0100

    secure call from other ejb

--- ejb-security-annotations/src/main/java/dmnlukasik/ejb/StatelessSession2Bean.java
+++ ejb-security-annotations/src/main/java/dmnlukasik/ejb/StatelessSession2Bean.java
@@ -2,8 +2,6 @@ package dmnlukasik.ejb;
 
 import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.RolesAllowed;
-import javax.annotation.security.RunAs;
-import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
@@ -20,22 +18,25 @@ import javax.ejb.Stateless;
  * which security-roles are allowed to access all the methods in the
  * EJB. @RolesAllowed may also be used at method-level.
  */
-@RolesAllowed({"superusers", "plainusers"})
-@RunAs("runasadmin")
-public class StatelessSession1Bean extends CommonStatelessSessionBean {
-
-    @EJB
-    private StatelessSession2Bean bean2;
+@RolesAllowed({"superusers", "plainusers", "runasadmin"})
+public class StatelessSession2Bean extends CommonStatelessSessionBean {
 
     public String greeting(final String name) {
-        System.out.println("*** StatelessSession1Bean.greeting");
+        System.out.println("*** StatelessSession2Bean.greeting");
         printSecurityInfo();
+
         try {
-            System.out.println("    Message for the superuser: " + bean2.superusersOnly());
-        } catch (Throwable theException) {
-            System.out.println("    No message for the superuser.");
+            Thread.sleep(2000L);
+        } catch (InterruptedException ignored) {
         }
-        String greetingFromBean2 = bean2.greeting(name);
-        return assembleGreeting(name, "StatelessSession1Bean") + "\n" + greetingFromBean2;
+
+        return assembleGreeting(name, "StatelessSession2Bean");
+    }
+
+    @RolesAllowed("superusers")
+    public String superusersOnly() {
+        System.out.println("*** StatelessSession2Bean.superusersOnly");
+        printSecurityInfo();
+        return "Bingo!";
     }
 }
